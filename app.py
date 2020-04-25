5cf6823f753e06b7e207fb6be3311c0f08ca514f
Xenia <lommy101@gmail.com>
2020-04-25 16:18:19 +0900

    add interactive shell

--- app.py
+++ app.py
@@ -17,3 +17,33 @@ def auth():
 #if __name__ == '__main__':
 #    app.run(host="localhost", port=5000)
 
+# paramiko
+import time
+import re
+
+def waitStreams(chan):
+    time.sleep(1)
+    outdata=errdata = ""
+ 
+    while chan.recv_ready():
+        outdata += str(chan.recv(1024).decode(encoding='UTF-8'))
+    while chan.recv_stderr_ready():
+        errdata += str(chan.recv_stderr(1024))
+ 
+    return outdata, errdata
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip, username=user, password=passwd)
+
+channel = ssh.invoke_shell()
+
+channel.send("")
+outdata, errdata = waitStreams(channel)
+print(re.compile(r'\x1b[^m]*m').sub('', outdata))
+
+while True:
+    command = input()
+    channel.send(command+"\n")
+    outdata, errdata = waitStreams(channel)
+    print(re.compile(r'\x1b[^m]*m').sub('', outdata))
