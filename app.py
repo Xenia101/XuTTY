0da723e8a92da5edf2928408b7aba8eac7e95ab6
XENIA <lommy101@gmail.com>
2020-05-03 22:06:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import paramiko
 import time
 import re
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+
 app = Flask(__name__)
 
 app.secret_key = b'WEVOSUE='
